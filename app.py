20506a1add634c638df061b600a1f40e1b7afe4b
Neeraj Sharma <nsharma0619@gmail.com>
2021-03-15 22:09:27 +0530

    'updated'

--- app.py
+++ app.py
@@ -22,43 +22,47 @@ dic_col = {'course_link':[],
 df = pd.DataFrame(dic_col)
 count=0
 for url in urls:
-    count+=1
-    dic={}
-    browser.open(url)
-    soup = browser.page
     try:
-        dic['url']=url
+        count+=1
+        dic={}
+        browser.open(url)
+        soup = browser.page
+        try:
+            dic['url']=url
+        except:
+            pass
+        try:
+            dic['course_link'] = soup.find('a', text='Go to course page')['href']
+        except:
+            pass
+        try:
+            dic['course_level'] = soup.find('dt', text='Level').findNext('dd').text
+        except:
+            pass
+        try:
+            dic['Application_Fee'] = soup.find('dt', text='Application Fee').findNext('dd').text.strip(' ').strip('\n')
+        except:
+            pass
+        
+        try:
+            course_date_tab = soup.find('div', id='tab-dates')
+            thead = course_date_tab.find('thead')
+            th = [i.text for i in thead.find_all('th')]
+            startdate_index = th.index('Term Start')
+            tbody = course_date_tab.find('tbody')
+            tr = tbody.find_all('tr')
+            td =[]
+            for i in tr:
+                try:
+                    td.append(i.find_all('td')[startdate_index].text.split('-')[1])
+                except:
+                    pass
+            dic['intake'] = list( dict.fromkeys(td) )
+        except:
+            pass
+        df = df.append(dic, ignore_index = True)
+        print(count)
     except:
-        pass
-    try:
-        dic['course_link'] = soup.find('a', text='Go to course page')['href']
-    except:
-        pass
-    try:
-        dic['course_level'] = soup.find('dt', text='Level').findNext('dd').text
-    except:
-        pass
-    try:
-        dic['Application_Fee'] = soup.find('dt', text='Application Fee').findNext('dd').text.strip(' ').strip('\n')
-    except:
-        pass
-    
-    try:
-        course_date_tab = soup.find('div', id='tab-dates')
-        thead = course_date_tab.find('thead')
-        th = [i.text for i in thead.find_all('th')]
-        startdate_index = th.index('Term Start')
-        tbody = course_date_tab.find('tbody')
-        tr = tbody.find_all('tr')
-        td =[]
-        for i in tr:
-            try:
-                td.append(i.find_all('td')[startdate_index].text.split('-')[1])
-            except:
-                pass
-        dic['intake'] = list( dict.fromkeys(td) )
-    except:
-        pass
-    df = df.append(dic, ignore_index = True)
-    print(count)
+        continue
+df.to_csv("first_half_info.csv")
 df.to_excel("first_half_info.xlsx")
